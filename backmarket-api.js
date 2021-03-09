b9ced08a6953000a33e66870921e8bceaf8831e3
adityawisnu <adityaferdiansyah33@gmail.com>
2021-03-09 15:04:53 +0700

    create git for api backmarket

---
+++ backmarket-api.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+var db = require("./config");
+
+db.connect(function(err){ 
+   if (err) throw err;
+
+app.get('/api', function (req, res){
+    var id = 'AND id LIKE \'%'+req.param('id')+'%\'';
+    var bmId = 'AND backmarket_id LIKE \'%'+req.param('backmarket_id')+'%\'';
+    var title = 'AND title LIKE \'%'+req.param('title')+'%\'';
+    var marketplace_cat_id = 'AND marketplace_category_id LIKE \'%'+req.param('marketplace_category_id')+'%\''
+    var getDataSQL = `SELECT * FROM backmarket_product WHERE 1 ${id} ${bmId} ${title} ${marketplace_cat_id}`;
+    db.query(getDataSQL.replace(/undefined/g, ''), async function(err, result){
+        var data = {
+            rows : result.length,
+            results : result
+        }
+        res.send(data)
+    });
+})
+
+app.get('/api/:category', function (req, res){
+    var Category = 'AND category_name LIKE \'%'+req.params.category+'%\'';
+    var id = 'AND id LIKE \'%'+req.param('id')+'%\'';
+    var bmId = 'AND backmarket_id LIKE \'%'+req.param('backmarket_id')+'%\'';
+    var title = 'AND title LIKE \'%'+req.param('title')+'%\'';
+    var marketplace_cat_id = 'AND marketplace_category_id LIKE \'%'+req.param('marketplace_category_id')+'%\''
+    var getDataSQL = `SELECT * FROM backmarket_product WHERE 1 ${Category} ${id} ${bmId} ${title} ${marketplace_cat_id}`;
+    db.query(getDataSQL.replace(/undefined/g, ''), async function(err, result){
+        var data = {
+            rows : result.length,
+            results : result
+        }
+        res.send(data)
+    });
+})
+
+app.listen(8000)
+console.log('Server started at http://127.0.0.1:8000/')
+
+})
